66c24162e3a61954f52fbc9ee51ceaf6a383e91e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-15 13:20:36 +0300

    update copy script

--- copy.sh
+++ copy.sh
@@ -3,3 +3,5 @@ cp -r $HOME/.config/i3 $PWD
 cp -r $HOME/.config/polybar $PWD
 cp -r $HOME/.config/rofi $PWD
 cp -r $HOME/.bashrc $PWD/bash/.bashrc
+cp -r $HOME/.config/tmux/tmux.conf $PWD/tmux/tmux.conf
+cp -r $HOME/.vimrc $PWD/vim/.vimrc
